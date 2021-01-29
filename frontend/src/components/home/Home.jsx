1a0a9641f84e94acc47f5757317037f1d5d1b11d
Rafael Cancian <cancian.rafa@gmail.com>
2021-01-29 10:57:40 +0000

    create template to frontend form

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Home"
+        subtitle="Project created to study react">
+        <div className='display-4'>Users form</div>
+        <hr />
+        <p className="mb-0">Create a new user</p>
+    </Main>
